bbacbc4124c14803e67eaa1524f45c39c2f44ac1
Christina <christina.russ@revature.com>
2020-06-30 11:49:30 -0500

    updated pizza class

--- Week 1/Java/Restaurant/src/com/revature/model/Pizza.java
+++ Week 1/Java/Restaurant/src/com/revature/model/Pizza.java
@@ -22,6 +22,55 @@ public class Pizza{
 	float price; //you DON'T have to initialize this with a value
 	
 	/*
+	 * Let's create our own constructor for the Pizza class. 
+	 */
+	
+	//A "no-args" constructor.
+	public Pizza() {
+		super(); //This is a call to the Object class's constructor.
+	}
+	
+	//A constructor using fields
+	public Pizza(int diameter, String topping, float price) {
+		this.diameter = diameter;
+		this.topping = topping;
+		this.price = price;
+	}
+	
+	/*
+	 * In order to create a method, we must define a method
+	 * signature. A method signature contains:
+	 * 
+	 * An Access Modifier
+	 * A Return Type (void, int, Object...)
+	 * A Name For the Method
+	 * A(n) (Optional) List of Parameters
+	 * A Potential List of Exceptions The Method Might Throw
+	 */
+	
+	/*
+	 * This method returns the calorie count for
+	 * a pizza based on its diameter.
+	 */
+	public void printPizzaNutrition() {
+		System.out.println("This pizza's topping is:" +
+				this.topping + " and its calories are:" 
+				+ this.diameter * 10);
+	}
+	
+	/*
+	 * Let's define a method that returns a value and defines 
+	 * one parameter.
+	 */
+	
+	public float addTopping(String extraTopping) {
+		if(extraTopping.equals("spinach")) {
+			return this.price + 1.0f;
+		}
+		else return this.price + 5.0f;
+	}
+	
+	/*
 	 * In fact, primitive data types have default values if
 	 * no value is supplied. Numeric types, for instance,
 	 * default to 0 or 0.0 while boolean for example defaults
@@ -55,17 +104,40 @@ public class Pizza{
 //		pepperoni = 8; DOES NOT COMPILE
 		pepperoni.diameter = 30;
 		pepperoni.topping = "Pepperoni";
+		pepperoni.price = 34.0f;
+		pepperoni.printPizzaNutrition(); //invoke method
+		
+		/*
+		 * The addTopping method returns a value to the caller.
+		 * The caller is the main method in this case as this
+		 * method (addTopping) is called within the main method.
+		 * 
+		 * We can store the value for later or immediately pass the
+		 * value to another method.
+		 */
+
+		float recalculatedPrice = pepperoni.addTopping("spinach");
+		System.out.println(pepperoni.addTopping("bacon"));
+		/*
+		 * Note that we did not modify the underlying
+		 * price of the pizza.
+		 */
+		System.out.println(pepperoni.price);
 		/*
 		 * When assigning a value to a float, if you
 		 * use a floating point value on the right side,
 		 * you must follow it with an "f".
 		 */
-		pepperoni.price = 34.0f;
+	
 		
 		Pizza sausage = new Pizza(); //yet another Pizza object
 		sausage.diameter = 50;
 		sausage.topping = "Sausage";
 		sausage.price = 17.0f;
+		sausage.printPizzaNutrition();
+		
+		Pizza supreme = new Pizza(10, "Supreme", 25.0f);
+		supreme.printPizzaNutrition();
 		
 		/*
 		 * What if we wanted to cast our pizza's diameter
