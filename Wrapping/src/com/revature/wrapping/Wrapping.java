5092c2b16767ac68f0e1c836f269383111d0c605
Nhurski <nhurski@gmail.com>
2020-07-02 10:59:11 -0400

    all

---
+++ Wrapping/src/com/revature/wrapping/Wrapping.java
@@ -0,0 +1,31 @@
+package com.revature.wrapping;
+
+public class Wrapping {
+	
+	
+	int i=5;
+	
+	 Integer i1= i;
+	 Float i2=(float)465.5;
+
+	 Integer i3= new Integer(76);
+
+
+public static void main (String ... args) {
+	
+	Wrapping wrap= new Wrapping();
+	System.out.println(wrap.i1);
+	System.out.println(wrap.i2);
+	System.out.println(wrap.i3);
+	
+	
+	
+	
+}
+
+
+
+
+
+
+}
